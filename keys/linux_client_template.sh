3c05d4ca729383c5d1bcc6ab62cee65b809bbb96
lobatolobato <felipelobato.rj@gmail.com>
2026-03-14 12:14:55 -0300

    test(linux-client-up): add validation tests for the up command on the linux client
    - Verifies that the vpn is up and working.
    
    - Verifies that the packet filters are setup.
    
    - Verifies that the routing rules are setup.
    
    - Verifies that the connection to the vpn from a process inside the cgroup is working

--- keys/linux_client_template.sh
+++ keys/linux_client_template.sh
@@ -6,6 +6,7 @@ ENDPOINT="$TEMPLATE_ENDPOINT"
 
 # If you dont know what is going on don't mess with these variables
 MTU="1420"
+MSS=$(($MTU - 40)) # (MTU - 40) works
 CGROUP="skel0vpn"
 CGROUP_PATH="/sys/fs/cgroup/net_cls/$CGROUP"
 CLASSID="0x10001"
@@ -14,6 +15,7 @@ MARK="11"
 TABLE="1221"
 SCRIPT_PATH=$(realpath "$0")
 INSTALL_PATH=/usr/local/bin/skel0vpn
+ENDPOINT_IP=$(ping -c 1 $ENDPOINT | awk -F '[()]' '/PING/ {print $2}')
 
 WIREGUARD_CONFIG_PATH=/etc/wireguard/$PROFILE.conf
 
@@ -137,9 +139,9 @@ PacketFilters() {
         echo "[#] Setting cgroup filter..."
         iptables -t mangle -A OUTPUT -m cgroup --cgroup $CLASSID -j MARK --set-mark $MARK
 
-        # Set MSS to MTU-40 to stop packets from being dropped
+        # Set MSS limit to stop packets from being dropped
         echo "[#] Setting tcp MSS limiter..."
-        iptables -t mangle -I OUTPUT -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss $(($MTU - 40))
+        iptables -t mangle -I OUTPUT -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss $MSS
 
         # Set nat to MASQUERADE so the vpn server doesn't see the wrong ip
         echo "[#] Setting up packet NAT mask..."
@@ -160,8 +162,6 @@ PacketFilters() {
     fi
 }
 RoutingRules() {
-    endpoint_ip=$(ping -c 1 $ENDPOINT | awk -F '[()]' '/PING/ {print $2}')
-
     echo "[Routing Rules]:"
     if [ "$1" == "set" ]; then
         # Packets with mark will go through the configured table
@@ -169,7 +169,7 @@ RoutingRules() {
         ip rule add fwmark $MARK priority 500 table $TABLE
         # Route vpn server's ip to main so it doesnt get routed to itself
         echo "[#] Packet Rule: route packets destined to the vpn server's endpoint through the main interface."
-        ip rule add to $endpoint_ip priority 400 table main
+        ip rule add to $ENDPOINT_IP priority 400 table main
         # Set the table as a route to the vpn
         echo "[#] Setting table $TABLE as a route to the vpn($PROFILE) interface..."
         ip route add default dev $PROFILE table $TABLE
@@ -185,7 +185,7 @@ RoutingRules() {
         ip rule del fwmark $MARK priority 500 2>/dev/null
 
         echo "[#] Deleting vpn server endpoint router"
-        ip rule del to $endpoint_ip priority 400 2>/dev/null
+        ip rule del to $ENDPOINT_IP priority 400 2>/dev/null
 
         echo "[#] Done."
     fi
@@ -294,13 +294,52 @@ fi
 
 #=================================== TESTS ===================================#
 test_echo_ok() {
-    echo -e "[#] \e[32m$1\e[0m"
+    for arg in "$@"; do
+        echo -e "[#] \e[32m$arg\e[0m"
+    done
+
 }
 test_echo_fail() {
-    echo -e "[#] \e[31m$1\e[0m"
+    for arg in "$@"; do
+        echo -e "[#] \e[31m$arg\e[0m"
+    done
 }
+
 check_owner() {
     [[ -z $(find "$1" \( ! -user "$2" -o ! -group "$3" \) -print -quit) ]]
+}
+to_int() {
+    printf '%ld' $1
+}
+to_uint() {
+    printf '%lu' $1
+}
+to_hex() {
+    printf '0x%x' $1
+}
+int_cmp() {
+    n1=$(to_int $1)
+    n2=$(to_int $2)
+
+    if [ "$n1" -gt "$n2" ]; then
+      return 1
+    elif [ "$n2" -gt "$n1" ]; then
+      return -1
+    else
+      return 0
+    fi
+}
+uint_cmp() {
+    n1=$(to_uint $1)
+    n2=$(to_uint $2)
+
+    if [ "$n1" -gt "$n2" ]; then
+      return 1
+    elif [ "$n2" -gt "$n1" ]; then
+      return -1
+    else
+      return 0
+    fi
 }
 
 test_install() {
@@ -345,6 +384,7 @@ test_install() {
     fi
 
     # CGroup creation and configuration
+    conf_classid=$(cat $CGROUP_PATH/net_cls.classid)
     if [ ! -d "/sys/fs/cgroup/net_cls" ]; then
         test_echo_fail "CGroup configuration and creation: FAILED"
         test_echo_fail "  net_cls cgroup is not mounted, probably because the directory was not created"
@@ -360,11 +400,11 @@ test_install() {
         if [$verbose == "true" ]; then
             test_echo_fail "$(ls -lR "$CGROUP_PATH")"
         fi
-    elif [ "$(printf '%d' $(cat $CGROUP_PATH/net_cls.classid))" != "$(printf '%d' $CLASSID)" ]; then
+    elif ! uint_cmp $conf_classid $CLASSID; then
         test_echo_fail "CGroup configuration and creation: FAILED"
         test_echo_fail "  $CGROUP cgroup's classid is wrong"
-        test_echo_fail "    Got: $(printf '0x%x' $(cat $CGROUP_PATH/net_cls.classid)) | $(printf '%d' $(cat $CGROUP_PATH/net_cls.classid))"
-        test_echo_fail "    Expected: $CLASSID | $(printf '%d' $CLASSID)"
+        test_echo_fail "    Got: $(to_hex $conf_classid) | $(to_uint $conf_classid)"
+        test_echo_fail "    Expected: $(to_hex $CLASSID) | $(to_uint $CLASSID)"
     else
       test_echo_ok "CGroup configuration and creation: OK"
     fi
@@ -437,6 +477,106 @@ test_uninstall() {
     fi
 }
 
+test_up() {
+    verbose=$1
+
+    echo "[#command:up]:"
+    # VPN up check
+    if ! wg show $PROFILE &>/dev/null; then
+        test_echo_fail "VPN up: FAILED"
+        test_echo_fail "  VPN is down."
+    elif ! curl -q4 --interface $PROFILE ifconfig.me &>/dev/null; then
+        test_echo_fail "VPN up: FAILED"
+        test_echo_fail "  VPN is up but trying to retrieve the tunnel's ip failed."
+        test_echo_fail "  test command was: curl -4 --interface $PROFILE ifconfig.me"
+    else
+        test_echo_ok "VPN up: OK"
+    fi
+
+    # Packet Filters
+    mangle=$(iptables -t mangle -S)
+    cgroup_filter_entry="-A OUTPUT -m cgroup --cgroup $(to_uint $CLASSID) -j MARK --set-xmark $(to_hex $MARK)/0xffffffff"
+    tcp_mss_limiter_entry="-A OUTPUT -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss $MSS"
+    nat_masquerade_entry="-A POSTROUTING -o $PROFILE -j MASQUERADE"
+
+    if ! grep -q -- "$cgroup_filter_entry" <<< "$mangle"; then
+        test_echo_fail "Packet Filters up (cgroup filter): FAILED"
+        test_echo_fail "  missing cgroup entry in mangle table"
+        test_echo_fail "  iptables -t mangle -S should list the entry bellow:"
+        test_echo_fail "    $cgroup_filter_entry"
+    else
+        test_echo_ok "Packet Filters up (cgroup filter): OK"
+    fi
+
+    if ! grep -q -- "$tcp_mss_limiter_entry" <<< "$mangle"; then
+        test_echo_fail "Packet Filters up (tcp mss limiter): FAILED"
+        test_echo_fail "  missing tcp mss limiter entry in mangle table"
+        test_echo_fail "  iptables -t mangle -S should list the entry bellow:"
+        test_echo_fail "    $tcp_mss_limiter_entry"
+    else
+        test_echo_ok "Packet Filters up (tcp mss limiter): OK"
+    fi
+
+    if ! grep -q -- "$nat_masquerade_entry" <<< "$(iptables-save -t nat)"; then
+        test_echo_fail "Packet Filters up (nat masquerade): FAILED"
+        test_echo_fail "  missing nat masquerade entry in nat table"
+        test_echo_fail "  iptables-save -t nat should list the entry bellow:"
+        test_echo_fail "    $nat_masquerade_entry"
+    else
+        test_echo_ok "Packet Filters up (nat masquerade): OK"
+    fi
+    # Routing Rules
+    ip_rules=$(ip rule show)
+    marked_packets_to_table_rule_entry="from all fwmark $(to_hex $MARK) lookup $TABLE"
+    table_to_interface_route_entry="default dev $PROFILE scope link"
+    outgoing_vpn_packets_rule_entry="from all to $ENDPOINT_IP lookup main"
+
+    if ! grep -q -- "$marked_packets_to_table_rule_entry" <<< "$ip_rules"; then
+        test_echo_fail "Routing Rules up (router[marked packets -> table]): FAILED"
+        test_echo_fail "  missing entry in ip rule list"
+        test_echo_fail "  ip rule show should list the entry bellow:"
+        test_echo_fail "    $marked_packets_to_table_rule_entry"
+    else
+        test_echo_ok "Routing Rules up (router[marked packets -> table]): OK"
+    fi
+
+    if ! grep -q -- "$outgoing_vpn_packets_rule_entry" <<< "$ip_rules"; then
+        test_echo_fail "Routing Rules up (router[outgoing vpn packets -> main interface]): FAILED"
+        test_echo_fail "  missing entry in ip rule list"
+        test_echo_fail "  ip rule show should list the entry bellow:"
+        test_echo_fail "    $outgoing_vpn_packets_rule_entry"
+    else
+        test_echo_ok "Routing Rules up (router[outgoing vpn packets -> main interface]): OK"
+    fi
+
+    if ! grep -q -- "$table_to_interface_route_entry" <<< "$(ip route show table $TABLE)"; then
+        test_echo_fail "Routing Rules up (route[table -> vpn interface]): FAILED"
+        test_echo_fail "  missing entry in ip route list"
+        test_echo_fail "  ip route show table $TABLE should list the entry bellow:"
+        test_echo_fail "    $table_to_interface_route_entry"
+    else
+        test_echo_ok "Routing Rules up (route[table -> vpn interface]): OK"
+    fi
+
+    # Connection test
+    cgroup_ip=$(cgexec -g net_cls:$CGROUP curl -s4 ifconfig.me)
+    tunnel_ip=$(curl -s4 --interface $PROFILE ifconfig.me)
+
+    if [ "$cgroup_ip" != "$tunnel_ip" ]; then
+        test_echo_fail "Connection to vpn from a process inside the cgroup: FAILED"
+        test_echo_fail "  cgroup ip is different from the tunnel ip"
+        test_echo_fail "  expected(tunnel): $tunnel_ip; got(cgroup): $cgroup_ip"
+    else
+        test_echo_ok "Connection to vpn from a process inside the cgroup: OK"
+    fi
+}
+test_down() {
+    verbose=$1
+}
+test_run() {
+    verbose=$1
+}
+
 if [ "$1" == "test" ]; then
     test="$2"
     verbose=false
@@ -450,11 +590,13 @@ if [ "$1" == "test" ]; then
     elif [ "$test" == "uninstall" ]; then
         test_uninstall verbose
     elif [ "$test" == "up" ]; then
-        # test_uninstall verbose
+        test_up verbose
     elif [ "$test" == "down" ]; then
-        # test_uninstall verbose
+        test_down verbose
+        echo "unimplemented"
     elif [ "$test" == "run" ]; then
-        # test_uninstall verbose
+        test_run verbose
+        echo "unimplemented"
     fi
 
     # up test
